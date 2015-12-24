f86bcbdf3509db0682141c5fea247a6711e27160
DarkishKnight <younggodsire@gmail.com>
2015-12-24 15:01:38 -0500

    Added more code to work with things. In process of making the buttons actually do what they need to do.

--- app/src/main/java/com/liam/realfirstdraft/SheetMusic.java
+++ app/src/main/java/com/liam/realfirstdraft/SheetMusic.java
@@ -2,12 +2,10 @@ package com.liam.realfirstdraft;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SheetMusic extends AppCompatActivity {
 
@@ -18,10 +16,10 @@ public class SheetMusic extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button backButton3 = (Button) findViewById(R.id.button3);
+        ImageButton backButton3 = (ImageButton) findViewById(R.id.backButton3);
 
         backButton3.setOnClickListener(
-                new Button.OnClickListener() {
+                new ImageButton.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Intent g = new Intent(getBaseContext(), MainActivity.class);
@@ -30,14 +28,6 @@ public class SheetMusic extends AppCompatActivity {
                 }
         );
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
